686fa41b85fd4aa0e167b2f95e8749a3aa5f2838
Elizaveta <isabethia7@gmail.com>
2023-03-16 19:27:20 +0300

    .

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 import './util.js';
-import { photoDescriptionArray } from './data.js';
-import { createElement } from './createMiniatures.js';
+//import { photoDescriptionArray } from './data.js';
+//import { createElement } from './createMiniatures.js';
 //import { openBigPicture } from './user-form.js';
 
-const finalPosts = photoDescriptionArray();
-createElement(finalPosts);
+//const finalPosts = photoDescriptionArray();
+//createElement(finalPosts);
 /*
 const finalPostsBig = photoDescriptionArray();
 openBigPicture(finalPostsBig);
