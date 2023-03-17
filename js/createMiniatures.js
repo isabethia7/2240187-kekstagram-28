eff6c3a641f79195b37fd95dc978bfeb3c246689
Elizaveta <isabethia7@gmail.com>
2023-03-17 22:52:05 +0300

    Add picture id

--- js/createMiniatures.js
+++ js/createMiniatures.js
@@ -1,24 +1,20 @@
 const pictureContainer = document.querySelector('.pictures');
 const pictureElement = document.querySelector('#picture').content.querySelector('.picture');
 
-const renderSimilarList = () => {
-  const similarPhotoPosts = document.createDocumentFragment();
+const similarPhotoPosts = document.createDocumentFragment();
 
-
-  similarPhotoPosts.forEach((item) => {
-    const postElement = pictureElement.cloneNode(true);
-    postElement.querySelector('.picture__img').src = item.urlAdress;
-    postElement.querySelector('.picture__comments').textContent = item.comment.length;
-    postElement.querySelector('.picture__likes').textContent = item.likes;
-    similarPhotoPosts.appendChild(postElement);
-  });
-  pictureContainer.append(similarPhotoPosts);
-};
-
-const clearSimilarList = () => {
-  renderSimilarList.innerHTML = '';
-};
-
-export {renderSimilarList, clearSimilarList};
-
+export function createElement(randomPostData) {
+  if (Array.isArray(randomPostData)) {
+    randomPostData.forEach((item) => {
+      const postElement = pictureElement.cloneNode(true);
+      postElement.setAttribute('data-picture-id', item.id);
+      postElement.querySelector('.picture__img').src = item.urlAdress;
+      postElement.querySelector('.picture__comments').textContent = item.comment.length;
+      postElement.querySelector('.picture__likes').textContent = item.likes;
+      similarPhotoPosts.appendChild(postElement);
+    });
+    pictureContainer.append(similarPhotoPosts);
+    return createElement();
+  }
+}
 
