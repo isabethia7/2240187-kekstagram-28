568f289047728bd4a6858f7693e570022b64665a
Elizaveta <isabethia7@gmail.com>
2023-03-19 02:49:52 +0300

    refact render main page

---
+++ js/renderMainPage.js
@@ -0,0 +1,20 @@
+export const pictureContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const thumbnail = document.createDocumentFragment();
+
+const renderThumbnail = (thumbnailData) => {
+  if (Array.isArray(thumbnailData)) {
+    thumbnailData.forEach((dataItem) => {
+      const thumbnailElement = thumbnailTemplate.cloneNode(true);
+      thumbnailElement.dataset.id = dataItem.id;
+      thumbnailElement.querySelector('.picture__img').src = dataItem.url;
+      thumbnailElement.querySelector('.picture__img').alt = dataItem.description;
+      thumbnailElement.querySelector('.picture__comments').textContent = dataItem.comments.length;
+      thumbnailElement.querySelector('.picture__likes').textContent = dataItem.likes;
+      thumbnail.appendChild(thumbnailElement);
+    });
+    pictureContainer.append(thumbnail);
+  }
+};
+
+export { renderThumbnail };
