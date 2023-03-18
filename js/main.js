0a19342208f11820804361bfe62e46812bb2cb23
Elizaveta <isabethia7@gmail.com>
2023-03-18 17:07:25 +0300

    unique picture

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
-//import './user-modal.js';
 import './user-modal.js';
 import { photoDescriptionArray } from './data.js';
 import { createElement } from './createMiniatures.js';
 
-const finalPosts = photoDescriptionArray();
+const finalPosts = photoDescriptionArray;
 createElement(finalPosts);
 
 export { finalPosts };
