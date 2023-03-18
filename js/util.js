fe05754f87bb016729b39c1b117e095c232b4a43
Elizaveta <isabethia7@gmail.com>
2023-03-19 02:24:32 +0300

    unic get

--- js/util.js
+++ js/util.js
@@ -1,4 +1,3 @@
-
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -6,9 +5,42 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
+const getUniqueValue = (min, max) => {
+  const previousValue = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValue.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValue.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValue.push(currentValue);
+    return currentValue;
+  };
+};
+
+
+const getRandomValue = (min, max) => {
+  const previousValue = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValue.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValue.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValue.push(currentValue);
+    return currentValue;
+  };
+};
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const isEnterKey = (evt) => evt.key === 'Enter';
 
-export { getRandomInteger, isEscapeKey, isEnterKey };
+export { getRandomInteger, getUniqueValue, getRandomValue, isEscapeKey, isEnterKey };
 
