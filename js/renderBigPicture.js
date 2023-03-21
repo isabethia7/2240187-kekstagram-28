0ccb51b6785e83289df7eb1a0aae150528e58326
Elizaveta <isabethia7@gmail.com>
2023-03-22 00:00:03 +0300

    add comment load button

--- js/renderBigPicture.js
+++ js/renderBigPicture.js
@@ -6,11 +6,14 @@ import { thumbnailsData } from './main.js';
 const bigPicture = document.querySelector('.big-picture');
 const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
 const bigPictureLikes = bigPicture.querySelector('.likes-count');
-const bigPictureComments = bigPicture.querySelector('.comments-count');
+//const bigPictureComments = bigPicture.querySelector('.comments-count');
 const cancelBigPicture = bigPicture.querySelector('.big-picture__cancel');
 const photoDescription = bigPicture.querySelector('.social__caption');
 const commentsCount = bigPicture.querySelector('.social__comment-count');
 const loadCommentsButton = bigPicture.querySelector('.comments-loader');
+let commentsPostAll = '';
+let commentShowCounter = 0;
+
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -20,21 +23,65 @@ const onDocumentKeydown = (evt) => {
   }
 };
 
+const commentCounterUpdate = () => {
+  commentsCount.textContent = `${commentShowCounter} из ${commentsPostAll}`;
+};
+
+const commentsRender = () => {
+  const commentsArray = Array.from(bigPicture.querySelectorAll('.social__comment'));
+  const commentsCounter = () => {
+    if ((commentsArray.length - commentShowCounter) < 5) {
+      commentShowCounter = commentShowCounter + (commentsArray.length - commentShowCounter);
+    } else {
+      commentShowCounter = commentShowCounter + 5;
+    }
+  };
+
+  const commentsShow = () => {
+    for (const comments of commentsArray) {
+      comments.classList.add('hidden');
+    }
+    const commentsHidden = commentsArray.slice(0, commentShowCounter);
+    for (const commentHidden of commentsHidden) {
+      commentHidden.classList.remove('hidden');
+    }
+  };
+
+  commentsCounter();
+
+  if (commentShowCounter < commentsArray.length) {
+    commentsShow();
+  } else {
+    commentsShow();
+    loadCommentsButton.classList.add('hidden');
+  }
+};
+
+const onCommentsLoaderButtonClick = () => {
+  commentsRender();
+  commentCounterUpdate();
+};
+
+loadCommentsButton.addEventListener('click', onCommentsLoaderButtonClick);
+
+
 const openBigPicture = (evt) => {
   if (evt.target.closest('.picture')) {
-    const target = evt.target.closest('.picture'); //выбираем из pictures - picture
-    const currentThumbnailData = thumbnailsData.find((item) => item.id === Number(target.dataset.id)); //ищем в нашем массиве с картинками тот элемент который равен по ID картинке по которой мы клинкули. ID картинки мы присваивали в createMiniatures.js строка 10
+    const target = evt.target.closest('.picture');
+    const currentThumbnailData = thumbnailsData.find((item) => item.id === Number(target.dataset.id));
     bigPicture.classList.remove('hidden');
     document.body.classList.add('modal-open');
-    commentsCount.classList.add('hidden');
-    loadCommentsButton.classList.add('hidden');
-
-    bigPictureImg.src = currentThumbnailData.url; //выбираем в big-picture - big-picture__img а в нем тэг img и меняяем ему значение src на адрес кортинки из нашего массива который получили ранее.
+    commentsCount.classList.remove('hidden');
+    loadCommentsButton.classList.remove('hidden');
+    bigPictureImg.src = currentThumbnailData.url;
     photoDescription.innerHTML = currentThumbnailData.description;
     bigPictureLikes.innerHTML = currentThumbnailData.likes;
-    bigPictureComments.innerHTML = currentThumbnailData.comments.length;
-
+    commentsPostAll = currentThumbnailData.comments.length;
+    //bigPictureComments.innerHTML = currentThumbnailData.comments.length;
     renderComment(currentThumbnailData.comments);
+
+    commentsRender();
+    commentCounterUpdate();
 
     document.addEventListener('keydown', onDocumentKeydown);
   }
@@ -44,6 +91,7 @@ const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
+  commentShowCounter = 5;
   clearComment();
 };
 
