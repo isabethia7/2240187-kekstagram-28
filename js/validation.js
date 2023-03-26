3894843181a1d588331e34dae10de928179cbf6a
Elizaveta <isabethia7@gmail.com>
2023-03-26 23:17:10 +0300

    fix prestine

--- js/validation.js
+++ js/validation.js
@@ -1,15 +1,16 @@
-
 export const imageUploadForm = document.querySelector('.img-upload__form');
-//const submitButton = imageUploadForm.querySelector('.img-upload__submit');
 export const hashtagInput = imageUploadForm.querySelector('.text__hashtags');
 export const commentInput = imageUploadForm.querySelector('.text__description');
 const maxHashtagCount = 5;
 const maxCommentCharacters = 140;
 
-
 export const pristine = new Pristine(imageUploadForm, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper',
+  classTo: 'img-upload__text',
+  errorClass: 'img-upload__text--invalid',
+  successClass: 'img-upload__text--valid',
+  errorTextParent: 'img-upload__text',
+  errorTextTag: 'span',
+  errorTextClass: 'img-upload__error',
 });
 
 function validateHashtag(hashtagString) {
@@ -35,7 +36,6 @@ function validateHashtagCount(hashtagString) {
   return true;
 }
 
-
 function validateUniqueHashtag(hashtagString) {
   const hashtagStringArray = hashtagString.trim().split(' ');
   const usedHashtags = {};
@@ -54,10 +54,26 @@ function validateComment(commentString) {
   return commentString.length <= maxCommentCharacters;
 }
 
-pristine.addValidator(hashtagInput, validateHashtag, 'Хэштег должен содержать только буквы и цифры. Хэштег должен начинаться с #. Хештег должен составлять не более 19 символов');
-pristine.addValidator(hashtagInput, validateHashtagCount, 'Может использоваться не более пяти хэштегов');
-pristine.addValidator(hashtagInput, validateUniqueHashtag, 'Один хэштег должен использоваться только один раз');
-pristine.addValidator(commentInput, validateComment, 'Комментарий должен содержать не более 140 символов');
+pristine.addValidator(
+  hashtagInput,
+  validateHashtag,
+  'Хэштег должен содержать только буквы и цифры. Хэштег должен начинаться с #. Хештег должен составлять не более 19 символов'
+);
+pristine.addValidator(
+  hashtagInput,
+  validateHashtagCount,
+  'Может использоваться не более пяти хэштегов'
+);
+pristine.addValidator(
+  hashtagInput,
+  validateUniqueHashtag,
+  'Один хэштег должен использоваться только один раз'
+);
+pristine.addValidator(
+  commentInput,
+  validateComment,
+  'Комментарий должен содержать не более 140 символов'
+);
 
 imageUploadForm.addEventListener('submit', (evt) => {
   if (!pristine.validate()) {
