ebcf5ee4bab1b35903a1925de77a5ccb22042ed3
Elizaveta <isabethia7@gmail.com>
2023-04-01 22:40:28 +0300

    refact download message

--- js/downoladMessage.js
+++ js/downoladMessage.js
@@ -1,34 +1,64 @@
-
-/*
-3.4. Если отправка данных прошла успешно, показывается соответствующее сообщение. Разметку сообщения, которая находится в блоке #success внутри шаблона template, нужно разместить перед закрывающим тегом </body>. Сообщение должно исчезать после нажатия на кнопку .success__button, по нажатию на клавишу Esc и по клику на произвольную область экрана за пределами блока с сообщением.
-
-3.5. Если при отправке данных произошла ошибка запроса, нужно показать соответствующее сообщение. Разметку сообщения, которая находится в блоке #error внутри шаблона template, нужно разместить перед закрывающим тегом </body>. Сообщение должно исчезать после нажатия на кнопку .error__button, по нажатию на клавишу Esc и по клику на произвольную область экрана за пределами блока с сообщением. В таком случае вся введённая пользователем информация сохраняется, чтобы у него была возможность отправить форму повторно.
-*/
-
+import { isEscapeKey } from './util.js';
+import { uploadPhotoSection, closeImageRedactor } from './uploadImage.js';
 const uploadMessageContainer = document.querySelector('body');
 const successTemplate = document.querySelector('#success').content.querySelector('.success');
 const errorTemplate = document.querySelector('#error').content.querySelector('.error');
 
+
 const closeSuccessWindow = () => {
   document.querySelector('.success').remove();
+  document.removeEventListener('click', outOfTheBox);
+  uploadMessageContainer.removeEventListener('keydown', onDocKeydown);
+  closeImageRedactor();
 };
 
 const closeErrorWindow = () => {
   document.querySelector('.error').remove();
+  document.removeEventListener('click', outOfTheBox);
+  uploadMessageContainer.removeEventListener('keydown', onDocKeydown);
 };
+
+function onDocKeydown (evt) {
+  const successWindow = document.querySelector('.success');
+  const errorWindow = document.querySelector('.error');
+  if (isEscapeKey(evt)) {
+    if (successWindow !== null) {
+      closeSuccessWindow();
+    } else if (errorWindow !== null) {
+      closeErrorWindow();
+    }
+  }
+}
+
+function outOfTheBox(evt) {
+  const successWindow = document.querySelector('.success');
+  const errorWindow = document.querySelector('.error');
+  if (evt.target === successWindow) {
+    closeSuccessWindow();
+  } else if (evt.target === errorWindow) {
+    closeErrorWindow();
+  }
+}
 
 const renderSuccessMessage = () => {
   const newSuccessTemplate = successTemplate.cloneNode(true);
   const successButton = newSuccessTemplate.querySelector('.success__button');
   successButton.addEventListener('click', closeSuccessWindow);
   uploadMessageContainer.append(newSuccessTemplate);
+  uploadMessageContainer.addEventListener('keydown', onDocKeydown);
+  uploadPhotoSection.classList.add('modal-open');
+  document.addEventListener('click', outOfTheBox);
 };
+
 
 const renderErrorMessage = () => {
   const newErrorTemplate = errorTemplate.cloneNode(true);
   const errorButton = newErrorTemplate.querySelector('.error__button');
   errorButton.addEventListener('click', closeErrorWindow);
   uploadMessageContainer.append(newErrorTemplate);
+  uploadMessageContainer.addEventListener('keydown', onDocKeydown);
+  uploadPhotoSection.classList.add('modal-open');
+  document.addEventListener('click', outOfTheBox);
 };
 
 export { renderSuccessMessage, renderErrorMessage };
