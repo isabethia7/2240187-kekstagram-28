98e57a0b5f379366a0ca224fd5403594ada37607
Elizaveta <isabethia7@gmail.com>
2023-04-06 21:21:21 +0300

    fix sort button highlight

--- js/sortPhotos.js
+++ js/sortPhotos.js
@@ -1,13 +1,14 @@
 import { reRenderAll } from './main.js';
 
 const showFiltersBarContainer = document.querySelector('main');
-const showFilterButton = showFiltersBarContainer.querySelector('.img-filters');
+const showFilterButtonContainer = showFiltersBarContainer.querySelector('.img-filters');
+const showFilterAllButtons = showFiltersBarContainer.querySelectorAll('.img-filters__button');
 const sortByDefaultButton = document.querySelector('#filter-default');
 const sortByRandomButton = document.querySelector('#filter-random');
 const sortByCommentsNumberButton = document.querySelector('#filter-discussed');
 const pictureCount = 10;
 
-showFilterButton.classList.remove('img-filters--inactive');
+showFilterButtonContainer.classList.remove('img-filters--inactive');
 
 const randomSort = () => Math.random() - 0.5;
 
@@ -16,23 +17,25 @@ const discussedSort = (a, b) => b.comments.length - a.comments.length;
 const defaultSort = (a, b) => a.id - b.id;
 
 function resetPictures () {
+  showFilterAllButtons.forEach((item) => item.classList.remove('img-filters__button--active'));
   const allPicture = document.querySelectorAll('.picture');
-  for (let i = 0; i < allPicture.length; i++) {
-    allPicture[i].remove();
-  }
+  allPicture.forEach((item) => item.remove());
 }
 
 const renderFilters = (data) => {
   sortByDefaultButton.addEventListener('click', () => {
     resetPictures();
+    sortByDefaultButton.classList.add('img-filters__button--active');
     reRenderAll(data.sort(defaultSort));
   });
   sortByRandomButton.addEventListener('click', () => {
     resetPictures();
+    sortByRandomButton.classList.add('img-filters__button--active');
     reRenderAll(data.sort(randomSort).slice(0, pictureCount));
   });
   sortByCommentsNumberButton.addEventListener('click', () => {
     resetPictures();
+    sortByCommentsNumberButton.classList.add('img-filters__button--active');
     reRenderAll(data.sort(discussedSort));
   });
 };
